3834a7794ee648424a6f04a50cfd7738cb64256f
Keng <kenglongmo@gmail.com>
2025-03-11 12:28:37 -0400

    begin

--- fizzBuzzandLetter.js
+++ fizzBuzzandLetter.js
@@ -1,2 +1,16 @@
 function fizzBuzz (n)
 {
+    //to make sure the number is up to 100 but not equal to it
+    if (!Number.isInteger(n) || n < 1 || n >= 100)
+  {
+    console.error("Error");
+    return;
+  }
+
+  for (let i = 1; i <= n; i++)
+  {
+    //will see if its able to be divided by those numbers
+    const remainderBy3 = i % 3;
+    const remainderBy5 = i % 5;
+
+    if()
