01cfcf41691a20341d7791e5cd90f8c321e0f9e6
Keng <kenglongmo@gmail.com>
2025-03-30 18:23:33 -0400

    errors

--- letterCount.js
+++ letterCount.js
@@ -1,4 +1,17 @@
 function letterCount (word)
 {
-  
+  if (typeof word !== "string")
+  {
+    console.error("Error");
+    return;
+  }
+
+  for (const letter of word)
+  {
+    if (!alphabet.includes(letter.toLowerCase()))
+    {
+      console.error("Error");
+      return;
+    }
+  }
 }
