7f63ebbcb7d97caa787419ec0c06ae7cde9b53ab
Keng <kenglongmo@gmail.com>
2025-04-03 23:02:27 -0400

    with 2 new?

--- caesarsTSP.js
+++ caesarsTSP.js
@@ -3,35 +3,35 @@ const shiftValue = 42;
 
 function encrypt (message)
   // Your encryption code here
-  {
-    
-    const index = alphabet.indexOf(message.toLowerCase());
-    const newIndex = (index + shiftValue) % alphabet.length;
-    return alphabet[newIndex];
-  
-  //step2
-
+{
     let encryptedMessage = "";
     for (let i = 0; i < message.length; i++)
-    {
-      encryptedMessage += encrypt(message[i], shiftValue);
-    }
+  {
+      //new var to let the message be nd will wrap around
+    let letter =message[i].toLowerCase();
 
+    if (alphabet.includes(letter)){
+    const index = alphabet.indexOf(letter());
+    //the letter it shifts to will be called new index a
+    const newIndex = (index + shiftValue) % alphabet.length;
+    encryptedMessage += alphabet[newIndex];
+    
     if ((i + 1) % 2 === 0) {
       const randomIndex = Math.floor(Math.random() * 26);
        encryptedMessage += alphabet[randomIndex];
     }
-
-    else {
-    encryptedMessage += char;
-    }
+  }
+    else{
+      encryptedMessage += letter;
+      }
+  }
     return encryptedMessage;
-  
 }
     
 function decrypt (encryptedMessage)
 {
   // Your decryption code here
-  
+  let decryptedMessage = '';
+
   return decryptedMessage;
 }
