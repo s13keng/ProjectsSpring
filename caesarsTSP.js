d1441fc825912ee73e93653b4f67e96643cc3bf6
Keng <kenglongmo@gmail.com>
2025-04-03 22:42:58 -0400

    ?

--- caesarsTSP.js
+++ caesarsTSP.js
@@ -4,23 +4,31 @@ const shiftValue = 42;
 function encrypt (message)
   // Your encryption code here
   {
+    
     const index = alphabet.indexOf(message.toLowerCase());
     const newIndex = (index + shiftValue) % alphabet.length;
     return alphabet[newIndex];
-  }
   
   //step2
-  function encryptMessage (word)
-  {
-    let encryptedMessage = "";
-    for (let i = 0; i < word.length; i++)
-    {
-      encryptedMessage += encrypt(word[i], shiftValue);
-    }
-    
-    return encryptedMessage;
-  }
 
+    let encryptedMessage = "";
+    for (let i = 0; i < message.length; i++)
+    {
+      encryptedMessage += encrypt(message[i], shiftValue);
+    }
+
+    if ((i + 1) % 2 === 0) {
+      const randomIndex = Math.floor(Math.random() * 26);
+       encryptedMessage += alphabet[randomIndex];
+    }
+
+    else {
+    encryptedMessage += char;
+    }
+    return encryptedMessage;
+  
+}
+    
 function decrypt (encryptedMessage)
 {
   // Your decryption code here
