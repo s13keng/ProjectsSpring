5d296017cd779d1da6dc0f4f8d7cc4172d7b4144
Keng <kenglongmo@gmail.com>
2025-04-25 11:14:29 -0400

    3done

---
+++ dom exercise/solution.js
@@ -0,0 +1,16 @@
+//Task 1
+function firstTask ()
+{
+	document.getElementById("task1").innerText = "Completed task1";
+}
+//Task2
+function secondTask ()
+{
+    document.getElementById("task2").innerHTML = "<button>Submit</button>"
+}
+//Task3
+function thirdTask ()
+{
+    document.body.style.backgroundColor = '#232323'
+}
+Tas
