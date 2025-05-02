5a6d359f48e07fda05a3c1c84856065ac07a06a0
Keng <kenglongmo@gmail.com>
2025-05-02 12:17:46 -0400

    notes

--- Event Exercise/script.js
+++ Event Exercise/script.js
@@ -1,15 +1,20 @@
+//step 1 ,get DOM content loaded to wrap it around
 document.addEventListener("DOMContentLoaded", function ()
 {
+    //step 2 ,elements from the Dom to work with
     const boxContainer = document.getElementById("box-container");
 	const newBoxButton = document.getElementById("new-box-button");
 	const colorForm = document.getElementById("color-form");
 	const colorInput = document.getElementById("color-input");
-    
+    //step 3 ,var to store values for each one
+    let boxColor = null; 
+	let boxIdCounter = 0; 
+
     //
 	function addNewBox ()
 	{
 		const box = document.createElement("div");
         //set it attribute to 
 		box.setAttribute("data-box-id", boxIdCounter.toString()); 
-        
+        box.
 }
