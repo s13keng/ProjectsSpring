49983440a2b4ee38814b4dd01f787c8696b9acc5
Keng <kenglongmo@gmail.com>
2025-05-02 12:14:06 -0400

    newbox

--- Event Exercise/script.js
+++ Event Exercise/script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function ()
 {
+    const boxContainer = document.getElementById("box-container");
+	const newBoxButton = document.getElementById("new-box-button");
+	const colorForm = document.getElementById("color-form");
+	const colorInput = document.getElementById("color-input");
+    
+    //
+	function addNewBox ()
+	{
+		const box = document.createElement("div");
+        //set it attribute to 
+		box.setAttribute("data-box-id", boxIdCounter.toString()); 
+        
 }
