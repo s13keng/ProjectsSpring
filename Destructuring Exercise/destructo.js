f1df84db0cc5e874241922ea10785e2b7b1436b9
Keng <kenglongmo@gmail.com>
2025-06-19 18:33:25 -0400

    console.log

--- Destructuring Exercise/destructo.js
+++ Destructuring Exercise/destructo.js
@@ -2,6 +2,8 @@
 const coordinates = {x: 34, y: 42, z: 67};
 // TODO: Destructo has found a map with coordinates marked on it. Use object destructuring to extract the `x` and `y` coordinates from the given `coordinates` object. Print the coordinates.
 const {x, y} = coordinates;
+console.log(`Coordinates found: x-axis =${x}, y-axis =${y}`);
+
 /* Task 2: The Map of Secrets */
 const locations = {
 	first: "Cave of Wonders",
@@ -10,7 +12,11 @@ const locations = {
 	fourth: "Desert of Shadows"
 };
 // TODO: The map reveals several locations, but only the first two are significant for the quest. Use object destructuring with the rest parameter to isolate the `first` and `second` locations from the `locations` object, capturing the rest in a variable called `remaining`. Print the key locations.
-
+const {
+	first,
+	second,
+	...remaining
+} = locations;
 /* Task 3: The Mysterious Door */
 const doorCode = {
 	upper: "Alpha",
