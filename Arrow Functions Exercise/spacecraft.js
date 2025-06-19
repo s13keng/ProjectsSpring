5c6ebc3fc4aa5ead327b1b2195b8d16136793445
Keng <kenglongmo@gmail.com>
2025-06-19 16:33:14 -0400

    done?

--- Arrow Functions Exercise/spacecraft.js
+++ Arrow Functions Exercise/spacecraft.js
@@ -12,11 +12,19 @@ const currentCoordinates = () => ({x: 45, y: 281, z: 115});
 console.log(currentCoordinates());
 /* Task 4: Understanding `this`: Message from Home Base */
 // TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
+
 const spacecraft = {
-	name: "King Seiko ",
+    name: 'King Seiko',
 	receiveMessage: (message) =>
-	{}
+	{
+        console.log(`Message received: ${message}`);
+        console.log(`This spacecraft is: ${this.name}`);
+    }
+
 };
+spacecraft.receiveMessage("Grand?!");
+
  /* Observations:
  * TODO: Explain here.
+ *In this contect to be able to call name of ship does not refer to the spacecraft object. Since arrow fuctions dont work with this so its technically  undefined and thats why is shows blank.
  */
