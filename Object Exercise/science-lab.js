b0ca81b8d446fdf3a72c547e971ad51fccf09500
Keng <kenglongmo@gmail.com>
2025-06-19 17:57:44 -0400

    half

--- Object Exercise/science-lab.js
+++ Object Exercise/science-lab.js
@@ -1,13 +1,25 @@
 /* Task 1: Compile Participant Details with Shorthand Property Names */
 // TODO: Construct an object named `participant` with properties for `name`, `age`, and `studyField`. Utilize shorthand property names to simplify your code.
+const name = "Taro Tanaka";
+const age = 56;
+const studyField = "Watch Designer"
 const participant = {
     name,
     age,
     studyField
 };
+console.log(`Participant:  ${name}, Age: ${age}, Study Field: ${studyField}`);
+
 /* Task 2: Implement a Shorthand Function for Participant Info */
 // TODO: Copy the `participant` object by adding a shorthand method named `displayInfo` that prints the participant's details using `this` and a template string.
-
+const participantCopy = {
+	...participant,
+	displayInfo ()
+	{
+		console.log(`Participant: ${this.name}, Age: ${this.age}, Study Field: ${this.studyField}`);
+	}
+};
+        participantCopy.displayInfo();
 /* Task 3: Implement a Same Shorthand Arrow Function for Participant Info */
 // TODO: Echo the above task with an arrow function. Observe the behavior of `this` and explain your findings.
 /*
