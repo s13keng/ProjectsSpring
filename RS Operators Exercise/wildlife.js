0593e7014aed4ee6e477f838ce8dd69eb821adb9
Keng <kenglongmo@gmail.com>
2025-06-19 17:37:02 -0400

    done

--- RS Operators Exercise/wildlife.js
+++ RS Operators Exercise/wildlife.js
@@ -34,9 +34,17 @@ const lionProfile = {
 	species: "Lion"
 };
 // TODO: Duplicate an animal profile object using a shallow copy. Add genetic diversity information using the `genetics` property to this copy. Observe and explain how changes to nested properties affect both the original and the copied object.
+const shallowCopy = {
+	...lionProfile,
+	genetics: "Diverse"
+};
+console.log('Original Lion Profile', lionProfile);
+console.log('Lion Genetic Profile', shallowCopy);
+/*
 /*
  * Observations:
  * TODO: Explain here.
+ * Genetics was only added to the shallow copy since its stored in a diffeerent reference.
  */
 
 /* Task 5: Analyze Ecosystem Health */
@@ -48,7 +56,17 @@ const ecosystemHealth = {
 	}
 };
 // TODO: You are given an object with a nested structure detailing the ecosystem's health, including water quality and food supply. Perform a shallow copy and modify a nested property. Observe and explain how changes to nested properties affect both the original and the copied object.
+const ecosystemCopy = {
+	...ecosystemHealth}
+	ecosystemCopy.foodSupply.herbivores = "Low";
+
+console.log("Original Ecosystem Health:", ecosystemHealth);
+console.log("Updated Ecosystem Health:", ecosystemCopy);
+
+
+
 /*
  * Observations:
  * TODO: Explain here.
+ * By having it outside of the scope for ecosystemCopy it ended up affecting both original and copy since of their reference.
  */
